4440212c9c82662ccd253531383ea97b55d108bd
maltelueken <maltelueken@users.noreply.github.com>
2025-12-15 16:24:11 +0000

    [ci skip] Add final exercise notebook eef533d21db885ef94e4e9fd7983ddd9d05c68c9

---
+++ _sources/python_scripts/final_exercise.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 📝 Exercise: Try out learned skills on Ames Housing dataset
+# The goal of this exercise is to apply all learned skills to a new dataset: The Ames Housing dataset.
+
+# We use this dataset in a regression setting to predict the sale prices of houses based on house features. That is, the goal is to predict the target `SalePrice` from numeric and/or categorical features in the dataset.
+
+# Remember to explore the dataset before building models. Then, start simple and step-by-step expand your approach to create better and better models.
+
+# You can load the data as follows:
+# %%
+import pandas as pd
+house_prices = pd.read_csv("../datasets/ames_housing_no_missing.csv")
+
+# %% [markdown]
+# **In case this exercise is difficult:**
+# To help you get started, you can take a look at the [description](datasets_ames_housing) of the dataset.
